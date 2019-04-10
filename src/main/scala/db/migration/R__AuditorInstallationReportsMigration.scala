bceb4dbee8b31e80049ff34d6469490c32e2a01c
Andy Doan <andy@foundries.io>
2019-04-10 09:37:29 -0500

    migrations: Allow audit service to be skipped
    Deployments of OTA Connect done by the OTA Community Edition project
    do not run an audit service.
    
    Fixes https://github.com/advancedtelematic/ota-device-registry/issues/84
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/main/scala/db/migration/R__AuditorInstallationReportsMigration.scala
+++ src/main/scala/db/migration/R__AuditorInstallationReportsMigration.scala
@@ -6,6 +6,7 @@ import com.advancedtelematic.libats.slick.db.AppMigration
 import com.advancedtelematic.ota.deviceregistry.client.AuditorHttpClient
 import com.advancedtelematic.ota.deviceregistry.db.MigrateOldInstallationReports
 import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 import slick.jdbc.MySQLProfile
 
 import scala.concurrent.Future
@@ -16,9 +17,15 @@ class R__AuditorInstallationReportsMigration extends AppMigration with ServiceHt
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   import system.dispatcher
 
+  private lazy val _log = LoggerFactory.getLogger(this.getClass)
+
   private val auditorUri = ConfigFactory.load().getString("auditor.uri")
   private val auditor = new AuditorHttpClient(auditorUri, defaultHttpClient)
 
   override def migrate(implicit db: MySQLProfile.api.Database): Future[Unit] =
-    new MigrateOldInstallationReports(auditor).run.map(_ => ())
+    if (auditorUri.length() > 0 ) {
+      new MigrateOldInstallationReports(auditor).run.map(_ => ())
+    } else {
+      _log.warn("Auditor API isn't enabled, skipping migration")
+    }
 }
